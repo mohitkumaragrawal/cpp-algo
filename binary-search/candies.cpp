1be83f1ce73e8b3c64f2358f2d9726b3625e1365
Mohit <mhtkrag@gmail.com>
2022-05-06 20:47:05 +0530

    changes

--- binary-search/candies.cpp
+++ binary-search/candies.cpp
@@ -4,7 +4,46 @@
 
 using namespace std;
 
+bool check(int* freq, int x) {
+  int max_length = 0;
+
+  bool foundOdd = false;
+  for (int i = 0; i < 26; ++i) {
+    if (freq[i] % 2 == 0) {
+      max_length += freq[i];
+    } else {
+      foundOdd = true;
+      max_length += freq[i] - 1;
+    }
+  }
+  if (foundOdd) max_length++;
+  return x <= max_length;
+}
+
+bool func(string& s, int x, int k) {
+  int freq[26];
+  for (int i = 0; i < 26; ++i) freq[i] = 0;
+  for (int i = 0; i < x; ++i) {
+    freq[s[i] - 'a']++;
+  }
+
+  if (check(freq, k)) {
+    return true;
+  }
+
+  for (int i = x; i < s.size(); ++i) {
+    freq[s[i] - 'a']++;
+    freq[s[i - x] - 'a']--;
+
+    if (check(freq, k)) return true;
+  }
+  return false;
+}
+
+#define endl '\n'
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   string s;
   cin >> s;
 
@@ -39,7 +78,22 @@ int main() {
     if (q > max_length) {
       cout << -1 << endl;
     } else {
-      cout << q << endl;
+      int low = q;
+      int high = s.size();
+      int ans = -1;
+
+      while (true) {
+        if (low > high) break;
+
+        int mid = low + (high - low) / 2;
+        if (func(s, mid, q)) {
+          ans = mid;
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      }
+      cout << ans << endl;
     }
   }
 }
