3236c2f10fb6e910b89a53c3615b373586b81f48
Mohit Kumar Agrawal <mhtkrag@gmail.com>
2023-02-14 15:10:06 +0530

    changes

--- tests/copilot.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define all(x) begin(x), end(x)
-#define OUT(T) cout << "Case #" << T << ": "
-using ll = long long;
-using lld = long double;
-using pll = pair<ll, ll>;
-using pii = pair<int, int>;
